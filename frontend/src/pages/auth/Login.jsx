2ab45f2e7add9699b415119e20365174c099d132
ashraf <sadarsign@gmail.com>
2025-04-12 21:17:07 +0300

    Initialized

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+    <h3 className='text-xl font-semibold text-black'>Wlcome Back</h3>
+    <p className="text-xs text-slate-700 mt-[5px] mb-6">
+      Please enter your details to log in
+    </p>
+      </div>
+    </div>
   )
 }
 
