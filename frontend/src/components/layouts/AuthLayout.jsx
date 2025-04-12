586daf46b71432bf8292815f31871d897024748d
ashraf <sadarsign@gmail.com>
2025-04-12 21:32:02 +0300

    Initialized

--- frontend/src/components/layouts/AuthLayout.jsx
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const AuthLayout = ({children}) => {
+const AuthLayout = ({ children }) => {
   return (
     <div>
-        <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
-            <h2 className="text-lg font-medium text-blac">Expense Tracker</h2>
-            {children}
-        </div>
+      <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
+        <h2 className="text-lg font-medium text-blac">Expense Tracker</h2>
+        {children}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
