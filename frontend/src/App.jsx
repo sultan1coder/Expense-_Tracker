e114742b0c7dde6270596a17cc5dc10f87dc1c32
ashraf <sadarsign@gmail.com>
2025-04-12 21:02:55 +0300

    Initialized

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,22 @@ Routes,
 Route,
 Navigate,
 } from "react-router-dom"
+import Login from './pages/auth/Login';
+import signUp from "./pages/auth/signUp";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
 const App = () => {
   return (
     <div>
       <Router>
         <Routes>
           <Route path='/' element={<Root />} />
-          <Route path='/login' exact element={<LoginForm />} />
+          <Route path='/login' exact element={<Login />} />
+          <Route path='/signUp' exact element={<signUp />} />
+          <Route path='/dashboard' exact element={<Home />} />
+          <Route path='/income' exact element={<Income />} />
+          <Route path='/expense' exact element={<Expense />} />
         </Routes>
       </Router>
     </div>
