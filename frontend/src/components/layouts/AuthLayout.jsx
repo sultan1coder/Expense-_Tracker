376a2cafb284b4b348203416153fe0a393730141
ashraf <sadarsign@gmail.com>
2025-04-13 21:12:23 +0300

    Initialized

--- frontend/src/components/layouts/AuthLayout.jsx
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -1,14 +1,23 @@
 import React from "react";
-
+import Card_2 from "../../assets/images/card2.png";
 const AuthLayout = ({ children }) => {
   return (
-    <div>
+    <div className="flex">
       <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
         <h2 className="text-lg font-medium text-blac">Expense Tracker</h2>
         {children}
       </div>
 
-      
+    <div className="hidden md:block w-[40vw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative ">
+      <div className="" />
+      <div className="" />
+      <div className="" />
+
+      <img src={Card_2}
+       className="" 
+       />
+    </div>
+
     </div>
   );
 };
