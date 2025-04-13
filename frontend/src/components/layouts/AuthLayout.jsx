18d75c0642e60f6ae9590b04b017df725b9744f7
ashraf <sadarsign@gmail.com>
2025-04-13 21:20:47 +0300

    Initialized

--- frontend/src/components/layouts/AuthLayout.jsx
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -9,8 +9,8 @@ const AuthLayout = ({ children }) => {
       </div>
 
     <div className="hidden md:block w-[40vw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative ">
-      <div className="" />
-      <div className="" />
+      <div className="w-48 h-48 rounded-[40px] bg-purple-600 absolute -top-7 -left-5  " />
+      <div className="w-48 h-56 rounded-[40px] border-[20px] border-furchsia-600 absolute top-[30%] -right-10 " />
       <div className="" />
 
       <img src={Card_2}
