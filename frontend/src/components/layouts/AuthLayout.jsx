3fa05903272647e5e717a67491051eea67248573
ashraf <sadarsign@gmail.com>
2025-04-15 20:34:32 +0300

    Initialized

--- frontend/src/components/layouts/AuthLayout.jsx
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -36,7 +36,7 @@ export default AuthLayout;
 
 
 const StatsInfoCard = ({icon, label, value, color}) => {
-  return <div className="">
+  return <div className="flex gap-6 bg-white p-4 rounded-xl shadow-md shadow-purple-400/10 border border-gray-200/50 z-10 ">
   <div className={`w-12 h-12 flex items-center justify-center text-[26px] text-white ${color} rounded-full drop-shadow-xl`} >
  
   {icon}
