45fe0c53b837a25b7959cd1b41645c4dd0c4c69b
ashraf <sadarsign@gmail.com>
2025-04-15 20:29:31 +0300

    Initialized

--- frontend/src/components/layouts/AuthLayout.jsx
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -33,3 +33,16 @@ const AuthLayout = ({ children }) => {
 };
 
 export default AuthLayout;
+
+
+const StatsInfoCard = ({icon, label, value, color}) => {
+  return <div className="">
+  <div className={`w-12 h-12 flex items-center justify-center text-[26px] text-white ${color} rounded-full drop-shadow-xl`} >
+ 
+  {icon}
+  </div>
+  <div>
+    
+  </div>
+  </div>
+}
