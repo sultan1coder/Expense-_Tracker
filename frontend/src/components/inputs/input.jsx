1573e8258a496ca40b5fbb135b5e7cfd3ac63f0e
ashraf <sadarsign@gmail.com>
2025-04-16 21:02:36 +0300

    Initialized

--- frontend/src/components/inputs/input.jsx
+++ frontend/src/components/inputs/input.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-const Input = () => {
+const Input = ({value, onChange, placeholder, label, type}) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassword);
+    }
   return (
     <div>Input</div>
   )
