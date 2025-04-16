2c1022b4a5ed81a7d2d1a6ef5b43fce20176f9ad
ashraf <sadarsign@gmail.com>
2025-04-16 21:12:03 +0300

    Initialized

--- frontend/src/components/inputs/input.jsx
+++ frontend/src/components/inputs/input.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {FaRegEye, FaRegEyeSlash} from "react-icons/fa6";
 
 const Input = ({value, onChange, placeholder, label, type}) => {
     const [showPassword, setShowPassword] = useState(false);
@@ -7,7 +8,18 @@ const Input = ({value, onChange, placeholder, label, type}) => {
         setShowPassword(!showPassword);
     }
   return (
-    <div>Input</div>
+    <div>
+        <label className='text-[13px] text-slate-800'>{label}</label>
+        <div className="input-box">
+            <input
+            type={type == "password" ? showPassword ? "text" : "password" : type}
+            placeholder={placeholder}
+            className='w-full bg-transparent outline-none'
+            value={value}
+            onChange={(e) => onChange(e)}
+             />
+        </div>
+    </div>
   )
 }
 
