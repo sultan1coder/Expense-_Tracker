30220126f349af7a485af29bf97585e7bbae75c6
ashraf <sadarsign@gmail.com>
2025-04-16 21:20:59 +0300

    Initialized

--- frontend/src/components/inputs/input.jsx
+++ frontend/src/components/inputs/input.jsx
@@ -18,6 +18,24 @@ const Input = ({value, onChange, placeholder, label, type}) => {
             value={value}
             onChange={(e) => onChange(e)}
              />
+
+             {type == "password" && (
+                <>
+                {showPassword ? (
+                <FaRegEye 
+                size={22}
+                className='text-primarry cursor-pointer'
+                onClick={() => toggleShowPassword}
+                />
+                ) : (
+                    <FaRegEyeSlash
+                    size={22}
+                    className='text-slate-400 cursor-pointer'
+                    onClick={() => toggleShowPassword}
+                     />
+                )}
+                </>
+             )}
         </div>
     </div>
   )
