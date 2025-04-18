3b457cf5e4967e35f943c969a343cb91d8e485ba
ashraf <sadarsign@gmail.com>
2025-04-18 22:43:10 +0300

    Initialized

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,38 +1,37 @@
-import React, {useState} from 'react'
-import AuthLayout from '../../components/layouts/AuthLayout'
-import {useNavigate} from "react-router-dom"
-import Input from '../../components/inputs/input';
+import React, { useState } from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import { useNavigate } from "react-router-dom";
+import Input from "../../components/inputs/input";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
-const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
-const navigate = useNavigate()
-
-const handleLogin = async (e) => {}
+  const handleLogin = async (e) => {};
 
   return (
     <AuthLayout>
       <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
-    <h3 className='text-xl font-semibold text-black'>Welcome Back</h3>
-    <p className="text-xs text-slate-700 mt-[5px] mb-6">
-      Please enter your details to log in
-    </p>
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Please enter your details to log in
+        </p>
 
-    <form onSubmit={handleLogin}>
-    <Input
-    value={email}
-    onChange={({target}) => setEmail(target.value)}
-    label="Email Address"
-    placeholder='sultan@gmail.com'
-    type='text'
-     />
-    </form>
+        <form onSubmit={handleLogin}>
+          <Input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="sultan@gmail.com"
+            type="text"
+          />
+        </form>
       </div>
     </AuthLayout>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
